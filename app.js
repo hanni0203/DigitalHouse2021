f72311624fe487d5b3e65102ba5e9d7609078b9f
hanni0203 <hanni.file@gmail.com>
2021-09-22 01:52:04 -0500

    Se creó formulario register, login, y el form en search bar, se creó el archivo stylesformulario.css

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { userInfo } = require('os');
 const app = express();
 
 const path = require('path');
@@ -10,3 +11,20 @@ app.listen(3000, (req, res) => console.log("servidor corriendo", "http:localhost
 app.use(express.static(path.resolve(__dirname, './public') ) );
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/home.html') ) );
+
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, './views/register.html') ) );
+
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, './views/login.html') ) );
+
+//metodo post
+app.post('/register', (req, res)=> {
+    res.redirect('/');
+});
+
+app.post('/login', (req, res)=> {
+    res.redirect('/');
+});
+
+app.post('/search', (req, res)=> {
+    res.redirect('/');
+});
